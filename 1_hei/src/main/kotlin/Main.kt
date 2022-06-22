a07f5240b7ba5c032b7df6645d26df6451c869b4
Pedro Arthur Freitas <pedro.afd@discente.ufma.br>
2022-06-22 14:10:58 -0300

    variaveis

--- 1_hei/src/main/kotlin/Main.kt
+++ 1_hei/src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
 fun main(args: Array<String>) {
-    println("What's your name?")
-    val name = readln()
-    println("oba, $name!")
+    println("oba pedro")
+
+    val maior = maxInt(10, 11);
+    println("o maior é: $maior")
+
+    val name1 : String = "Pedro"
+    var name2 = "Anna"
+    name2 = name2.plus(" Clara")
+    println(name2)
+
 }
+
+fun maxInt(a: Int, b:Int) = if(a > b) a else b
+
+//Concepts:
+/*
+1.top level variables
+2.
+ */
